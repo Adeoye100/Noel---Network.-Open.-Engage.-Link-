036457f7d7a9f8c9057bcd46e539f677db7bb50d
Adeoye Opeyemi <adeoyeopeyemi951@gmail.com>
2025-10-20 15:38:32 +0100

    fix: Add firebase.json hosting configuration

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-// When deploying to GitHub Pages, the assets need to be prefixed with the repository name.
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
   output: 'export', // Enables static HTML export
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  // No assetPrefix or basePath needed for Firebase Hosting
 };
 
 module.exports = nextConfig;
